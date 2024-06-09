54eb1df73ae434999591b7a4bb98a108391c43d0
Josh <josh@bonnick.dev>
2024-06-09 16:42:14 +0100

    rewrite file

--- main.py
+++ main.py
@@ -8,7 +8,12 @@ from php_file_parser import PHPFileParser
 def main(filename: string, convert_to: string):
     file = PHPFileParser(filename)
 
-    variables = list(map(lambda variable: getattr(CaseConverter(variable), convert_to)(), file.variables()))
+    new_content = file.content()
+
+    for variable in file.variables():
+        new_content = new_content.replace(variable, getattr(CaseConverter(variable), convert_to)())
+
+    print(new_content)
 
 
 if __name__ == '__main__':
