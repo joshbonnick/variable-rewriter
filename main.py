07f9ac3e4e7e4746802f7a067165a16235b6e3d8
Josh <josh@bonnick.dev>
2024-06-09 16:12:23 +0100

    conversion option from cli

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ from php_variable_finder import PHPVariableFinder
 if __name__ == '__main__':
     finder = PHPVariableFinder(sys.argv[1])
 
-    print(finder.variables())
+    if len(sys.argv) > 2:
+        convert_to = sys.argv[2]
+    else:
+        convert_to = 'camel'
 
-    for variable in finder.variables():
-        converter = CaseConverter(variable)
-        print(converter.snake())
+    variables = list(map(lambda variable: getattr(CaseConverter(variable), convert_to)(), finder.variables()))
+
+    print(variables)
