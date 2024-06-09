22c3cc10a33b5f935c7b4fba2be270a578b1a2bf
Josh <josh@bonnick.dev>
2024-06-09 19:44:01 +0100

    improve test suite

--- tests/test_case_converter.py
+++ tests/test_case_converter.py
@@ -6,36 +6,44 @@ import rewriter
 class TestCaseConverter(TestCase):
     def test_lower_case_conversion(self):
         converter = rewriter.CaseConverter("some_CASEexample")
-        assert converter.lower() == "some_caseexample"
+        assert converter.lower() == "some_caseexample", f'Returned: [{converter.lower()}]'
 
     def test_upper_case_conversion(self):
         converter = rewriter.CaseConverter("some_CASEeXaMple")
-        assert converter.upper() == "SOME_CASEEXAMPLE"
+        assert converter.upper() == "SOME_CASEEXAMPLE", f'Returned: [{converter.upper()}]'
 
     def test_camel_case_conversion(self):
         converter = rewriter.CaseConverter("snake_case_example")
-        assert converter.camel() == "SnakeCaseExample"
+        assert converter.camel() == "snakeCaseExample", f'Returned: [{converter.camel()}]'
+
+    def test_camel_case_conversion_from_camel_case(self):
+        converter = rewriter.CaseConverter("CamelCaseExample")
+        assert converter.camel() == "camelCaseExample", f'Returned: [{converter.camel()}]'
+        converter = rewriter.CaseConverter("camelCaseExample")
+        assert converter.camel() == "camelCaseExample", f'Returned: [{converter.camel()}]'
 
     def test_snake_case_conversion(self):
         converter = rewriter.CaseConverter("CamelCaseExample")
-        assert converter.snake() == "camel_case_example"
+        assert converter.snake() == "camel_case_example", f'Returned: [{converter.snake()}]'
 
     def test_single_word_camel_case(self):
         converter = rewriter.CaseConverter("word")
-        assert converter.camel() == "Word"
+        assert converter.camel() == "word", f'Returned: [{converter.camel()}]'
+        converter = rewriter.CaseConverter("two_word")
+        assert converter.camel() == "twoWord", f'Returned: [{converter.camel()}]'
 
     def test_single_word_snake_case(self):
         converter = rewriter.CaseConverter("Word")
-        assert converter.snake() == "word"
+        assert converter.snake() == "word", f'Returned: [{converter.snake()}]'
 
     def test_empty_string_camel_case(self):
         converter = rewriter.CaseConverter("")
-        assert converter.camel() == ""
+        assert converter.camel() == "", f'Returned: [{converter.camel()}]'
 
     def test_empty_string_snake_case(self):
         converter = rewriter.CaseConverter("")
-        assert converter.snake() == ""
+        assert converter.snake() == "", f'Returned: [{converter.snake()}]'
 
     def test_snake_case_when_starting_with_capital(self):
         converter = rewriter.CaseConverter("SnakeCase")
-        assert converter.snake() == "snake_case"
+        assert converter.snake() == "snake_case", f'Returned: [{converter.snake()}]'
