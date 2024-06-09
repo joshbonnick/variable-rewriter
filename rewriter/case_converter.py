f0e7725447354cd0452d1b58b5a1da6e76bbc37a
Josh <josh@bonnick.dev>
2024-06-09 19:52:42 +0100

    bug: camel case conversion

--- rewriter/case_converter.py
+++ rewriter/case_converter.py
@@ -1,3 +1,6 @@
+from re import findall
+from re import match
+from re import split
 from re import sub
 
 
@@ -13,9 +16,23 @@ class CaseConverter:
         return self.variable_delimiter + subject
 
     def camel(self) -> str:
-        return self._as_variable("".join(x.capitalize() for x in self.subject.lower().split("_")))
+        if len(self.subject) == 0:
+            return self.subject
+
+        # Check if the string is already in camel case
+        if self._is_camel_case():
+            return self.subject
+
+        words = findall(r'[A-Za-z][a-z]*', self.subject) if match(r'^[A-Za-z]+$', self.subject) else split(r'[\s_-]+', self.subject)
+
+        self.subject = words[0].lower() + ''.join(word.capitalize() for word in words[1:])
+
+        return self._as_variable(self.subject)
 
     def snake(self) -> str:
+        if len(self.subject) == 0:
+            return self.subject
+
         return self._as_variable(sub("(?<!^)(?=[A-Z])", "_", self.subject).lower())
 
     def upper(self) -> str:
@@ -23,3 +40,6 @@ class CaseConverter:
 
     def lower(self) -> str:
         return self._as_variable(self.subject.lower())
+
+    def _is_camel_case(self) -> bool:
+        return match(r'^[a-z][a-zA-Z]*$', self.subject) and not match(r'^[a-z]+(?:[A-Z][a-z]*)*$', self.subject)
