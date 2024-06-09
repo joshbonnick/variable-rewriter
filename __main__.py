a633960c986addc1f87ebde7af7d3ac3d931ea74
Josh <josh@bonnick.dev>
2024-06-09 19:33:45 +0100

    import all from module

--- __main__.py
+++ __main__.py
@@ -1,12 +1,12 @@
 import sys
 
-import rewriter
+from rewriter import *
 
 
 def main(file_name: str, method: str):
-    parser = rewriter.PHPFileParser(file_name)
-    converter = rewriter.VariableConverter(parser, method, '$')
-    rewriter.FileWriter(file_name).write(converter.new_content)
+    parser = PHPFileParser(file_name)
+    converter = VariableConverter(parser, method, '$')
+    FileWriter(file_name).write(converter.new_content)
 
 
 if __name__ == '__main__':
